d2f19ea0af03932adb800a4f7ba05f5cb413813a
Kelvin Young <kelvinKelvinyoung@Kelvins-MacBook-Pro.local>
2016-10-21 20:26:59 +0200

    Clean up code spacing

--- index.php
+++ index.php
@@ -2,62 +2,62 @@
 	include 'header.php';
 ?>
 
-<section>
-	<div class="slider-container-fluid container-fluid">
-    	<div class="slider-container container ybafadeIn">
-        	<div class="slider-info col-sm-12 col-md-6">
-            	<h2>Simple.</h2>
-				<h3>Compact Hoisting Solutions</h3>
-				<p>GEDA-Dechentreiter GmbH & Co. KG is a leading producer of construction hoists worldwide operating for over eight decades.</p> 
-                <a class="btn" href="/contact-us.html">Contact Us</a>
-            </div>
-            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/simple-slide-img.png" class="img-center img-responsive"/></div>
-        </div>
-        <div class="slider-container container hidden ybafadeIn">
-        	<div class="slider-info col-sm-12 col-md-6">
-            	<h2>Smart.</h2>
-				<h3>Compact Hoisting Solutions</h3>
-				<p>The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish chutes as well as ladder hoists and safety systems for hoists.</p> 
-                <a class="btn" href="/contact-us.html">Contact Us</a>
-            </div>
-            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/smart-slide-img.png" class="img-center img-responsive"/></div>
-        </div>
-        <div class="slider-container container hidden ybafadeIn">
-        	<div class="slider-info col-sm-12 col-md-6">
-            	<h2>Easy.</h2>
-				<h3>Compact Hoisting Solutions</h3>
-				<p> Innovative solutions for industrial elevators and building maintenance units, DESTRA offers sales, servicing and hire of equipment.</p> 
-                <a class="btn" href="/contact-us.html">Contact Us</a>
-            </div>
-            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/easy-slide-img.png" class="img-center img-responsive"/></div>
-        </div>
-    </div>
-    <div class="home-section-content container-fluid">
-    	<div class="container">
-        	<h1>Simple. Smart. Easy.</h1>
-            <h3>Compact Hoisting Solutions</h3>
-    	</div>
-        <div class="container padding-top padding-bottom">
-        	<div class="col-sm-12 col-md-6">
-            	<iframe width="470" height="276" src="https://www.youtube.com/embed/kLYqYxUbpEg" frameborder="0" allowfullscreen></iframe>
-            </div>
-            <div class="col-sm-12 col-md-6">
-            	<iframe width="470" height="276" src="https://www.youtube.com/embed/45E0wTNHURE" frameborder="0" allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="container">
-        	<p>GEDA-Dechentreiter GmbH & Co. KG is a leading producer of construction hoists worldwide operating for over eight decades. The company’s headquarters is situated in Asbach-Bäumenheim, Bavaria, Southern Germany, with additional subsidiaries in Germany, Russia, the USA and Turkey and a comprehensive dealer network.</p>
-
- 
-
-<p>DESTRA was established to offer the quality driven GEDA products to the South African market. The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish chutes as well as ladder hoists and safety systems for hoists. Added to this are innovative solutions for industrial elevators and building maintenance units.</p>
-
- 
-
-<p>DESTRA offers sales, servicing and hire of equipment.</p>
-        </div>
-    </div>
-</section>
+                <section>
+                	<div class="slider-container-fluid container-fluid">
+                    	<div class="slider-container container ybafadeIn">
+                        	<div class="slider-info col-sm-12 col-md-6">
+                            	<h2>Simple.</h2>
+                				<h3>Compact Hoisting Solutions</h3>
+                				<p>GEDA-Dechentreiter GmbH & Co. KG is a leading producer of construction hoists worldwide operating for over eight decades.</p> 
+                                <a class="btn" href="/contact-us.html">Contact Us</a>
+                            </div>
+                            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/simple-slide-img.png" class="img-center img-responsive"/></div>
+                        </div>
+                        <div class="slider-container container hidden ybafadeIn">
+                        	<div class="slider-info col-sm-12 col-md-6">
+                            	<h2>Smart.</h2>
+                				<h3>Compact Hoisting Solutions</h3>
+                				<p>The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish              chutes as well as ladder hoists and safety systems for hoists.</p> 
+                                <a class="btn" href="/contact-us.html">Contact Us</a>
+                            </div>
+                            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/smart-slide-img.png" class="img-center img-responsive"/></div>
+                        </div>
+                        <div class="slider-container container hidden ybafadeIn">
+                        	<div class="slider-info col-sm-12 col-md-6">
+                            	<h2>Easy.</h2>
+                				<h3>Compact Hoisting Solutions</h3>
+                				<p> Innovative solutions for industrial elevators and building maintenance units, DESTRA offers sales, servicing and hire of equipment.</p> 
+                                <a class="btn" href="/contact-us.html">Contact Us</a>
+                            </div>
+                            <div class="slider-img col-sm-12 col-md-6 hidden-xs hidden-sm"><img src="img/easy-slide-img.png" class="img-center img-responsive"/></div>
+                        </div>
+                    </div>
+                    <div class="home-section-content container-fluid">
+                    	<div class="container">
+                        	<h1>Simple. Smart. Easy.</h1>
+                            <h3>Compact Hoisting Solutions</h3>
+                    	</div>
+                        <div class="container padding-top padding-bottom">
+                        	<div class="col-sm-12 col-md-6">
+                            	<iframe width="470" height="276" src="https://www.youtube.com/embed/kLYqYxUbpEg" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                            	<iframe width="470" height="276" src="https://www.youtube.com/embed/45E0wTNHURE" frameborder="0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="container">
+                        	<p>
+                                GEDA-Dechentreiter GmbH & Co. KG is a leading producer of construction hoists worldwide operating for over eight decades. The company’s headquarters is situated in Asbach-Bäumenheim, Bavaria, Southern Germany, with additional subsidiaries in Germany, Russia, the USA and Turkey and a comprehensive dealer network.
+                            </p>
+                            <p>
+                                DESTRA was established to offer the quality driven GEDA products to the South African market. The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish chutes as well as ladder hoists and safety systems for hoists. Added to this are innovative solutions for industrial elevators and building maintenance units.
+                            </p>
+                            <p>
+                                DESTRA offers sales, servicing and hire of equipment.
+                            </p>
+                        </div>
+                    </div>
+                </section>
 
 <?php
 	include 'footer.php';
