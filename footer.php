e68542d5afb54122392e90c55594b88b48be1d95
Kelvin Young <kelvinKelvinyoung@Kelvins-MacBook-Pro.local>
2016-10-21 20:35:48 +0200

    Cleand up footer code

--- footer.php
+++ footer.php
@@ -1,37 +1,49 @@
-<footer class="container-fluid">
-	<div class="container">
-    	<div class="col-sm-12 col-md-3">
-        	<h3 class="footer-heading">ABOUT.</h3>
-            <p>DESTRA was established to offer the quality driven GEDA products to the South African market. The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish chutes as well as ladder hoists and safety systems for hoists. Added to this are innovative solutions for industrial elevators and building maintenance units.</p>
-
-<p>DESTRA offers sales, servicing and hire of equipment.</p>
-        </div>
-        <div class="col-sm-12 col-md-3">
-        	<h3 class="footer-heading">CONTACT US.</h3>
-            <p><strong>Address:</strong> 84 Western Services Road, Wendywood, Johannesburg</p>
-<p><strong>Contact:</strong> Remo Torrente</p>
-<p><strong>Email:</strong> remo@destra.co.za</p>
-<p><strong>Phone:</strong> 083 255 3415</p>
-
-<strong>Proud GEDA Partner</strong>
-			</p>
-        </div>
-        <div class="col-sm-12 col-md-3">
-        	<h3 class="footer-heading">CONNECT.</h3>
-            <?php
-                include "contact_form.php";
-            ?>
-        </div>
-        <div class="col-sm-12 col-md-3">
-        	<h3 class="footer-heading">DIRECTIONS.</h3>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7167.505236342482!2d28.08766!3d-26.07434!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e9572ecbf6adca1%3A0x4e36235b874ce04!2s84+Western+Service+Rd%2C+Sandton%2C+2148%2C+South+Africa!5e0!3m2!1sen!2sus!4v1464764333715" style="border:0" allowfullscreen="" frameborder="0" height="300" width="300"></iframe>
-        </div>
-    </div>
-    <div class="container">
-     	<div class="copyright"><p class="copyright-info">Copyright © <span id="year">2016</span> Destra - Compact Hoisting Solutions. All Rights Reserved. Designed by Your Brand Agency</p>
-    	</div>
-    </div>
-</footer>
-
-</body>
-</html>
+                <footer class="container-fluid">
+                	<div class="container">
+                    	<div class="col-sm-12 col-md-3">
+                        	<h3 class="footer-heading">ABOUT.</h3>
+                            <p>
+                                DESTRA was established to offer the quality driven GEDA products to the South African market. The wide range of products comprises, amongst others, rope hoists, rack and pinion hoists, suspended working platforms, mast climbing platforms and rubbish chutes as well as ladder hoists and safety systems for hoists. Added to this are innovative solutions for industrial elevators and building maintenance units.
+                            </p>
+                            <p>
+                                DESTRA offers sales, servicing and hire of equipment.
+                            </p>
+                        </div>
+                        <div class="col-sm-12 col-md-3">
+                        	<h3 class="footer-heading">
+                                CONTACT US.
+                            </h3>
+                            <p>
+                                <strong>Address:</strong> 84 Western Services Road, Wendywood, Johannesburg
+                            </p>
+                            <p>
+                                <strong>Contact:</strong> Remo Torrente
+                            </p>
+                            <p>
+                                <strong>Email:</strong> remo@destra.co.za
+                            </p>
+                            <p>
+                                <strong>Phone:</strong> 083 255 3415
+                            </p>
+                            <p>
+                                <strong>Proud GEDA Partner</strong>
+                            </p>
+                        </div>
+                        <div class="col-sm-12 col-md-3">
+                        	<h3 class="footer-heading">CONNECT.</h3>
+                            <?php
+                                include "contact_form.php";
+                            ?>
+                        </div>
+                        <div class="col-sm-12 col-md-3">
+                        	<h3 class="footer-heading">DIRECTIONS.</h3>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7167.505236342482!2d28.08766!3d-26.07434!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e9572ecbf6adca1%3A0x4               e36235b874ce04!2s84+Western+Service+Rd%2C+Sandton%2C+2148%2C+South+Africa!5e0!3m2!1sen!2sus!4v1464764333715" style="border:0" allowfullscreen="" frameborder="0" height="300" width="300"></iframe>
+                        </div>
+                    </div>
+                    <div class="container">
+                     	<div class="copyright"><p class="copyright-info">Copyright © <span id="year">2016</span> Destra - Compact Hoisting Solutions. All Rights Reserved. Designed by Your Brand Agency</p>
+                    	</div>
+                    </div>
+                </footer>
+        </body>
+    </html>
